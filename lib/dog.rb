8aa02b85bd90c8005dd2a82a0a7ad226aefa32cc
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-10 16:38:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 # dog.rb
 class Dog 
   def initialize(this_dog_name)
-    @this_dog_name = name 
+    @this_dog_name = dog_name 
   end
   
   def name 
     this_dog_name
   end 
 end 
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+puts lassie.name 
